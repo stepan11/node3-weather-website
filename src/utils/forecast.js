5dd60917f4879196250ce1afebac4c2c52d5ce81
stepan11 <stepanuch953@gmail.com>
2021-06-23 17:10:52 +0300

    The humidiry has been added

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -11,7 +11,7 @@ const forecast = (latitude, longitude, callback) => {
     } else {
       callback(
         undefined,
-        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degress out.`
+        `${body.current.weather_descriptions[0]}. It is currently ${body.current.temperature} degress out. It feels like ${body.current.feelslike} degress out. There is ${body.current.humidity}% of humidity`
       );
     }
   });
